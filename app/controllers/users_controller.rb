96de54986f7704afd8627c2045cb8fc219f5fdda
McBainCO <grahammcbain@gmail.com>
2014-09-03 16:16:06 -0600

    more things work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @user
+    @users = User.all
   end
 
   def new
@@ -9,7 +9,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(allowed_params)
+    if
+      @user.save
+      flash[:notice] = "Your have registered Successfuly"
+      redirect_to root_path
+    end
+
 
   end
 
 end
+
+  private
+
+  def allowed_params
+    params.require(:user).permit(:name, :email)
+
+
+  end
+
+
