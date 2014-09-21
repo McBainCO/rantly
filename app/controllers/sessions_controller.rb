4d26470d4dc221e1cbb9a4b0bc21afcddeb59daa
McBainCO <grahammcbain@gmail.com>
2014-09-21 11:08:22 -0700

    sessions are getting started

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,21 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
-end
+  end
 
   def create
       @user = User.find_by(email: params[:user][:email])
       if @user && @user.password == params[:user][:password]
         session[:user_id] = @user.id
+        redirect_to root_path
+        flash[:notice] = 'Your have logged in Sucessfully'
       else
         render :new
       end
   end
+
+  def destroy
+    #destroy session
+  end
+
 end
