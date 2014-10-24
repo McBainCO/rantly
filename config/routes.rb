4d5291e68cc3b1e8ba84ffff473d9e011a57b5fd
McBainCO <grahammcbain@gmail.com>
2014-10-24 12:26:26 -0600

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root "users#hompage"
+  root "users#homepage"
   resources :users
   resources :sessions, :only => [:new, :edit, :destroy, :create]
 
